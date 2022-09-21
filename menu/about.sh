812508f5dabdad518a3ae3a1f51d44da70a3dc9d
Zea King Store <98459024+Zeastore@users.noreply.github.com>
2022-09-21 07:14:54 +0700

    Update about.sh

--- menu/about.sh
+++ menu/about.sh
@@ -3,7 +3,7 @@ MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
 CEKEXPIRED () {
     today=$(date -d +1day +%Y-%m-%d)
-    Exp1=$(curl -sS https://raw.githubusercontent.com/sibeesans/ajsc/main/anjay/allow | grep $MYIP | awk '{print $3}')
+    Exp1=$(curl -sS https://raw.githubusercontent.com/Zeastore/cv/main/anjay/allow | grep $MYIP | awk '{print $3}')
     if [[ $today < $Exp1 ]]; then
     echo -e "\e[32mSTATUS SCRIPT AKTIF...\e[0m"
     else
@@ -11,7 +11,7 @@ CEKEXPIRED () {
     exit 0
 fi
 }
-IZIN=$(curl -sS https://raw.githubusercontent.com/sibeesans/ajsc/main/anjay/allow | awk '{print $4}' | grep $MYIP)
+IZIN=$(curl -sS https://raw.githubusercontent.com/Zeastore/cv/main/cuk/allow | awk '{print $4}' | grep $MYIP)
 if [ $MYIP = $IZIN ]; then
 echo -e "\e[32mPermission Accepted...\e[0m"
 CEKEXPIRED
@@ -21,7 +21,7 @@ exit 0
 fi
 clear
 echo -e "\e[36m╒════════════════════════════════════════════╕\033[0m"
-echo -e " \E[0;41;36m                 Bagoes Vpn Service                \E[0m"
+echo -e " \E[0;41;36m                 DXTXTR VPN SERVICE                \E[0m"
 echo -e "\e[36m╘════════════════════════════════════════════╛\033[0m"
 echo ""
 read -n 1 -s -r -p "Press any key to back on menu"
